6668a456edc4e88cd3b8c19b5625ec38578bf35f
SophieM <47410186+SophM@users.noreply.github.com>
2019-03-29 17:54:58 -0700

    typo corrected for 'res.sendFile' -works better without an 's'- in htmlRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,27 +14,27 @@ var path = require("path");
 module.exports = function(app) {
     // get request to display the home page
     app.get("/", function(req, res) {
-        res.sendFiles(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
     // get request to display the cocktail-survey page
     app.get("/cocktail-survey", function(req, res) {
-        res.sendFiles(path.join(__dirname, "../public/cocktailSurvey.html"));
+        res.sendFile(path.join(__dirname, "../public/cocktailSurvey.html"));
     });
 
     // get request to display the beer-survey page
     app.get("/beer-survey", function(req, res) {
-        res.sendFiles(path.join(__dirname, "../public/beerSurvey.html"));
+        res.sendFile(path.join(__dirname, "../public/beerSurvey.html"));
     });
 
     // get request to display the wine-survey page
     app.get("/wine-survey", function(req, res) {
-        res.sendFiles(path.join(__dirname, "../public/wineSurvey.html"));
+        res.sendFile(path.join(__dirname, "../public/wineSurvey.html"));
     });
 
     // get request to display the home page if no matching route is found
     app.get("*", function(req, res) {
-        res.sendFiles(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
 }; 
