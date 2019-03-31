12bd7544f66a5472d91381543ecc3f5477719a29
SophieM <47410186+SophM@users.noreply.github.com>
2019-03-30 17:13:36 -0700

    add updated beerSurvey.html and beers.js - rephrase the questions and options and correct the score of the beers

--- app/data/beers.js
+++ app/data/beers.js
@@ -3,11 +3,10 @@
 // --------------------------------------------------- 
 
 // question
-// 1. light
-// 2. easy-drinking
-// 3. bitter
-// 4. fruity
-// 5. sour
+// A. 1-strong / 5-light
+// B. 1- not bitter / 5-bitter
+// C. 1-not fruity / 5-fruity
+// D. 1- not sour / 5-sour
 
 // big array of objects to hold all the beer data
 // = catalog of beers to compare the beer-survey results to
@@ -16,8 +15,7 @@ var beerCatalog = [
         name: "Guiness Draught",
         picture: "https://www.irishcentral.com/uploads/article/126375/cropped_GUINNESS.jpg?t=1528116858",
         score: [
-            2,
-            1,
+            3,
             3,
             1,
             1,
@@ -29,10 +27,9 @@ var beerCatalog = [
         picture: "https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/174/images/215/lambicframboise12oz__18888.1350074045.1280.1280.jpg?c=2",
         score: [
             5,
-            3,
             1,
             5,
-            3
+            1,
         ]
 
     },
@@ -41,7 +38,6 @@ var beerCatalog = [
         picture: "https://www.totalwine.com/media/sys_master/twmmedia/hf8/h7e/8806398689310.png",
         score: [
             3,
-            2,
             2,
             1,
             1,
@@ -52,11 +48,10 @@ var beerCatalog = [
         name: "Grapefruit Sculpin",
         picture: "https://beerconnoisseur.com/sites/default/files/styles/beer_page_245w/public/beer/ballast-point-grapefruit-sculpin.jpg?itok=V20G16iO",
         score: [
-            1,
             2,
             5,
             4,
-            1
+            2,
         ]
 
     },
@@ -65,10 +60,9 @@ var beerCatalog = [
         picture: "https://beerboard.com/uploads/brand_image/cover/113/Bottleshot_60.jpg",
         score: [
             2,
-            3,
             4,
             1,
-            1
+            1,
         ]
 
     },
@@ -76,11 +70,10 @@ var beerCatalog = [
         name: "Sofie",
         picture: "https://img.saveur-biere.com/img/p/26918-42852-v4_product_img.jpg",
         score: [
-            1,
-            3,
             2,
             2,
-            1
+            2,
+            2,
         ]
 
     },
@@ -89,10 +82,9 @@ var beerCatalog = [
         picture: "http://www.totalwine.com/media/sys_master/twmmedia/h69/h1e/10027628691486.png",
         score: [
             1,
-            1,
             2,
+            3,
             5,
-            5
         ]
 
     },
@@ -101,10 +93,9 @@ var beerCatalog = [
         picture: "http://breakside.com/wp-content/uploads/2018/03/PFSA@2x-min.png",
         score: [
             3,
-            4,
             1,
             5,
-            5
+            5,
         ]
 
     },
